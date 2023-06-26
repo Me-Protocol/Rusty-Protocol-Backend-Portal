ee1c4ead3243758e4e8bfc2ed7a0103adfbdfb7b
Thelle Codes <boltdsg@gmail.com>
2023-06-26 13:26:01 +0300

    update

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,21 +1,22 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
-import { Brand } from "@src/globalServices/brand/entities/brand.entity";
-import { Category } from "@src/globalServices/category/entities/category.entity";
-import { Customer } from "@src/globalServices/customer/entities/customer.entity";
-import { Device } from "@src/globalServices/user/entities/device.entity";
-import { User } from "@src/globalServices/user/entities/user.entity";
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Brand } from '@src/globalServices/brand/entities/brand.entity';
+import { Category } from '@src/globalServices/category/entities/category.entity';
+import { Customer } from '@src/globalServices/customer/entities/customer.entity';
+import { Device } from '@src/globalServices/user/entities/device.entity';
+import { User } from '@src/globalServices/user/entities/user.entity';
+require('dotenv').config();
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: "postgres",
+  type: 'postgres',
   host: process.env.POSTGRES_HOST,
   port: parseInt(process.env.POSTGRES_PORT, 10),
   username: process.env.POSTGRES_USER,
   database: process.env.POSTGRES_DATABASE,
   password: process.env.POSTGRES_PASSWORD,
   entities: [User, Device, Customer, Brand, Category],
-  migrations: [__dirname + "/../database/migrations/*{.ts,.js}"],
+  migrations: [__dirname + '/../database/migrations/*{.ts,.js}'],
   extra: {
-    charset: "utf8mb4_unicode_ci",
+    charset: 'utf8mb4_unicode_ci',
   },
   synchronize: true,
   autoLoadEntities: true,
