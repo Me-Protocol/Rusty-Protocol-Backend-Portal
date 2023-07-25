3bcfd63809cdc5a800e50cfffd9791ecfd9a9179
John <45017704+codemobii@users.noreply.github.com>
2023-07-25 23:04:16 +0100

    fix deployment

--- src/globalServices/logger/logger.service.ts
+++ src/globalServices/logger/logger.service.ts
@@ -1,11 +1,9 @@
-import { Injectable } from "@nestjs/common";
-import { WinstonModule } from "nest-winston";
-import { loggerFormat, loggerTransport } from "./logger.module";
+import { Injectable } from '@nestjs/common';
+import { WinstonModule } from 'nest-winston';
+import { loggerFormat, loggerTransport } from './logger.module';
 
 @Injectable()
-export class LoggerService {
-  // extendable redis cache service goes here
-}
+export class LoggerService {}
 
 export const logger = WinstonModule.createLogger({
   format: loggerFormat,
