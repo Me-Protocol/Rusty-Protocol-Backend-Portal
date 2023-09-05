d70d3c1955e577c6fb92b243fb84a89f68ea372c
John <45017704+codemobii@users.noreply.github.com>
2023-09-05 17:10:13 +0100

    Update controller.ts

--- src/modules/storeManagement/offers/controller.ts
+++ src/modules/storeManagement/offers/controller.ts
@@ -52,6 +52,7 @@ export class OfferManagementController {
   ) {
     const brandId = req.user.brand.id;
     body.brandId = brandId;
+    console.log('body', body);
     return await this.offerManagementService.updateOffer(offerId, body);
   }
 
