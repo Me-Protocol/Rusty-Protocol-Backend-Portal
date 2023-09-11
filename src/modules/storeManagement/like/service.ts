1b6d09e001537e38bc6ad1989e634fc75aadd01b
John <45017704+codemobii@users.noreply.github.com>
2023-09-11 10:24:16 +0100

    Update service.ts

--- src/modules/storeManagement/like/service.ts
+++ src/modules/storeManagement/like/service.ts
@@ -102,6 +102,7 @@ export class LikeManagementService {
 
       return createdLike;
     } catch (error) {
+      console.log(error);
       logger.error(error);
       throw new HttpException(error.message, 500, {
         cause: new Error(error.message),
