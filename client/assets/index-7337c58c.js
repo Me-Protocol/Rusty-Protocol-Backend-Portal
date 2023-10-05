93de5f93eda30c2f35676ae3b707abc93f91c0b3
John <45017704+codemobii@users.noreply.github.com>
2023-10-05 23:24:20 +0100

    prod build

--- client/assets/index-7337c58c.js
+++
@@ -1,1 +0,0 @@
-import{L as i}from"./Login-c075524e.js";import"./index-e858156d.js";import"./formik.esm-3bf747de.js";import"./chunk-K7XRJ7NL-0eace40d.js";export{i as default};
---
+++ client/assets/index-b2437ef4.js
@@ -0,0 +1,1 @@
+import{M as a}from"./MyProfile-b17fe8d7.js";import"./index-2780a68e.js";export{a as default};
