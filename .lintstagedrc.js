491d9b72938159555e2551c043f06fd4f384fb6f
fk <57292038+agentCruz@users.noreply.github.com>
2023-10-06 10:06:00 +0100

    feat(clean commit): configured husky and commit lint for uniformity

---
+++ .lintstagedrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  '*.{ts,tsx}': 'npm run lint:fix',
+  'package.json': 'npx fixpack', // Sort package.json
+  '*.md': 'markdownlint',
+  '*.json': 'npx jsonlint',
+  '*.yml': 'yamllint',
+  'COMMIT_EDITMSG': 'npx commitlint --edit',
+};
