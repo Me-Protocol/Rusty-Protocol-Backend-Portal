206f51a08f13fcd9535076db2389f223fb18fc08
fk <57292038+agentCruz@users.noreply.github.com>
2023-10-07 22:56:21 +0100

    feat: aws pipeline config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   // '*.{ts}': 'npm run lint:fix',
-  'package.json': 'npx fixpack', // Sort package.json
   '*.md': 'markdownlint',
   '*.json': 'npx jsonlint',
   '*.yml': 'yamllint',
