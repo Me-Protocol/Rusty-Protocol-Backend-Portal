122406577fc61cdcabcb44ce91f8fdb3d84fc736
John <45017704+codemobii@users.noreply.github.com>
2023-10-12 09:15:05 +0100

    dev build

---
+++ client/assets/chunk-W7A7QDAK-6d8480b1.js
@@ -0,0 +1,1 @@
+import{j as o,O as l}from"./index-49b376e0.js";var s=({size:e="2rem",...r})=>o.jsx(l,{borderRadius:"full",boxSize:e,...r});s.displayName="SkeletonCircle";export{s as S};
--- client/assets/index-41c0a01f.js
+++
@@ -1,1 +0,0 @@
-import{M as a}from"./MyProfile-48c16812.js";import"./index-e858156d.js";export{a as default};
