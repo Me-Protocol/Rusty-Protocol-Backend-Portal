6f85ba634db548d867344dd90f17fe7d2b9cf02a
Joseph Nwokotubo <jcool4liv@gmail.com>
2023-11-10 20:00:25 +0100

    update client

--- client/assets/index-c4a7687b.js
+++
@@ -1,1 +0,0 @@
-import{M as a}from"./MyProfile-f51184a0.js";import"./index-c853c0cb.js";export{a as default};
---
+++ client/assets/index-f3148c58.js
@@ -0,0 +1,1 @@
+import{M as a}from"./MyProfile-84acfae7.js";import"./index-4d742258.js";export{a as default};
