cff84fcc448d6a7a16f9c7358bb6d964c330dc9c
Joseph Nwokotubo <jcool4liv@gmail.com>
2023-11-22 08:54:53 +0100

    feat: udpate client

--- client/assets/index-107918aa.js
+++
@@ -1,1 +0,0 @@
-import{L as a}from"./Login-baa1d517.js";import"./index-d1513471.js";import"./formik.esm-d4019cef.js";import"./index.esm-cdcb8747.js";import"./chunk-I5V4ORUK-3a25ad6f.js";export{a as default};
--- client/assets/index-d2142816.js
+++
@@ -1,1 +0,0 @@
-import{M as a}from"./MyProfile-2805e1fe.js";import"./index-d1513471.js";export{a as default};
