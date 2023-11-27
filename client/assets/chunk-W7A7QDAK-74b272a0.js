be8892ec37c553d3c0d0ea22e2c7f819a6ec06fc
Joseph <joseph@Joes-MacBook-Pro.local>
2023-11-27 07:05:16 +0100

    feat: update client

--- client/assets/chunk-W7A7QDAK-74b272a0.js
+++
@@ -1,1 +0,0 @@
-import{j as o,v as l}from"./index-d7824e35.js";var s=({size:e="2rem",...r})=>o.jsx(l,{borderRadius:"full",boxSize:e,...r});s.displayName="SkeletonCircle";export{s as S};
---
+++ client/assets/index-9553f5c8.js
@@ -0,0 +1,1 @@
+import{M as a}from"./MyProfile-c4009c18.js";import"./index-d2f88df5.js";export{a as default};
