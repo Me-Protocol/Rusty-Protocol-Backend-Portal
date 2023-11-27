5cf638099240b61516ff585686a1c15a392cc66f
Joseph <joseph@Joes-MacBook-Pro.local>
2023-11-27 11:01:33 +0100

    feat: fix backend

---
+++ client/assets/index-3e4bbc45.js
@@ -0,0 +1,1 @@
+import{L as a}from"./Login-d492c075.js";import"./index-1596cd59.js";import"./formik.esm-ead150db.js";import"./index.esm-bc85d5fb.js";import"./chunk-I5V4ORUK-5439af09.js";export{a as default};
---
+++ client/assets/index-b879e9f6.js
@@ -0,0 +1,1 @@
+import{M as a}from"./MyProfile-c38bb1d9.js";import"./index-1596cd59.js";export{a as default};
