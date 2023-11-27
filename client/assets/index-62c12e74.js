dabf991823514d8c952725714dcec87f552584c1
Joseph <joseph@Joes-MacBook-Pro.local>
2023-11-27 07:06:20 +0100

    feat: udpate client

---
+++ client/assets/index-62c12e74.js
@@ -0,0 +1,1 @@
+import{L as a}from"./Login-c7d1f5c2.js";import"./index-d2f88df5.js";import"./formik.esm-3879454d.js";import"./index.esm-ab6c4e6e.js";import"./chunk-I5V4ORUK-688dab42.js";export{a as default};
--- client/assets/index-b594be2d.js
+++
@@ -1,1 +0,0 @@
-import{M as a}from"./MyProfile-7fa069e2.js";import"./index-bdfb93cd.js";export{a as default};
