921c634e85e4d22439cef0d7dde1c69e93b33201
Joseph <joseph@Joes-MacBook-Pro.local>
2023-11-29 16:12:06 +0100

    update client (6)

--- client/assets/index-368f8041.js
+++
@@ -1,1 +0,0 @@
-import{M as a}from"./MyProfile-ee8c5e7a.js";import"./index-dcfd6306.js";export{a as default};
---
+++ client/assets/index-4d6d97c8.js
@@ -0,0 +1,1 @@
+import{L as a}from"./Login-74375b60.js";import"./index-d3251126.js";import"./formik.esm-c23b5f63.js";import"./index.esm-fb84a6b4.js";import"./chunk-I5V4ORUK-eaf4d24e.js";export{a as default};
