26dbc8f0b46b9472c1e7e76ef8d3f60611e18250
Joseph <joseph.nwokotubo@unotelos.com>
2024-02-09 12:03:26 +0100

    fix

--- src/modules/accountManagement/brandAccountManagement/socket/brand-upload.gateway.ts
+++ src/modules/accountManagement/brandAccountManagement/socket/brand-upload.gateway.ts
@@ -5,7 +5,7 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway({ cors: false })
 export class BrandUploadGateway implements OnGatewayConnection {
   @WebSocketServer()
   server: Server;
