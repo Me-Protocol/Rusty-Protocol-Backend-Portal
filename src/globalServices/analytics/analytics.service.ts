7d9b75bbea805f2e356db92828db777ec6e1f7a9
John <45017704+codemobii@users.noreply.github.com>
2024-02-16 13:19:31 +0100

    fix: analytics (redemption on other brand)

--- src/globalServices/analytics/analytics.service.ts
+++ src/globalServices/analytics/analytics.service.ts
@@ -416,18 +416,15 @@ export class AnalyticsService {
       where: {
         createdAt: Between(start, end),
         brandId: brandId,
-        offer: {
-          rewardId: In(rewardIds),
-        },
+        redeemRewardId: In(rewardIds),
       },
     });
 
     const total = await this.orderRepo.count({
       where: {
         createdAt: Between(start, end),
-        offer: {
-          rewardId: In(rewardIds),
-        },
+        brandId,
+        redeemRewardId: In(rewardIds),
       },
     });
 
@@ -470,21 +467,27 @@ export class AnalyticsService {
 
     const rewardIds = rewards.map((reward) => reward.id);
 
+    // const brandOffers = await this.offerRepo.find({
+    //   where: {
+    //     brandId,
+    //   },
+    // });
+
+    // const brandOfferIds = brandOffers.map((offer) => offer.id);
+
     const orders = await this.orderRepo.find({
       where: {
         createdAt: Between(start, end),
-        offer: {
-          rewardId: In(rewardIds),
-        },
+        brandId: Not(brandId),
+        redeemRewardId: In(rewardIds),
       },
     });
 
     const total = await this.orderRepo.count({
       where: {
         createdAt: Between(start, end),
-        offer: {
-          rewardId: In(rewardIds),
-        },
+        brandId: Not(brandId),
+        redeemRewardId: In(rewardIds),
       },
     });
 
