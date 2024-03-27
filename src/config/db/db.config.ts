be84ae9749fff2f78c992baad5282a46919587f4
John <45017704+codemobii@users.noreply.github.com>
2024-03-27 11:46:13 +0100

    fix brand onboard

--- src/config/db/db.config.ts
+++ src/config/db/db.config.ts
@@ -33,8 +33,8 @@ import {
         entities: ['dist/**/*.entity{.ts,.js}'],
         migrations: [join(__dirname, '../dist/migrations/**.js')],
         subscribers: [join(__dirname, '../dist/subscribers/**.js')],
-        migrationsRun: RUN_DEFAULT_MIGRATION || false,
-        synchronize: RUN_DB_SYNC || false,
+        migrationsRun: true,
+        synchronize: true,
         autoLoadEntities: true,
         ssl: {
           rejectUnauthorized: false,
