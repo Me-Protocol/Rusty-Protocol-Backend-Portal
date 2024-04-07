afa620d782dd491c3ee83cc058bedad2a9020b6b
John <45017704+codemobii@users.noreply.github.com>
2024-04-08 00:21:23 +0100

    Update 1712528947949-migrations.ts

--- src/migrations/1712528947949-migrations.ts
+++ src/migrations/1712528947949-migrations.ts
@@ -4,44 +4,44 @@ export class Migrations1712528947949 implements MigrationInterface {
   name = 'Migrations1712528947949';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "auditTrail" DROP COLUMN "userEmail"`);
-    await queryRunner.query(`ALTER TABLE "auditTrail" DROP COLUMN "userName"`);
-    await queryRunner.query(
-      `ALTER TABLE "coupon" ADD "order_code" character varying`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "coupon" ADD "brandDiscountId" character varying`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "coupon" ADD "brandPriceRuleId" character varying`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "order" ALTER COLUMN "orderCode" SET DEFAULT substr(uuid_generate_v4()::text, 1, 6)`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" TYPE numeric`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" SET DEFAULT '0.984'`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "admin_settings" ALTER COLUMN "topupLimitFactor" SET DEFAULT '1.2'`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "admin_settings" ALTER COLUMN "meTokenValue" SET DEFAULT '0.09'`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "auditTrail" ALTER COLUMN "userId" SET NOT NULL`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "auditTrail" ALTER COLUMN "auditType" SET NOT NULL`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "auditTrail" ALTER COLUMN "description" SET NOT NULL`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "auditTrail" ALTER COLUMN "reportableId" SET NOT NULL`,
-    );
+    // await queryRunner.query(`ALTER TABLE "auditTrail" DROP COLUMN "userEmail"`);
+    // await queryRunner.query(`ALTER TABLE "auditTrail" DROP COLUMN "userName"`);
+    // await queryRunner.query(
+    //   `ALTER TABLE "coupon" ADD "order_code" character varying`,
+    // );
+    // await queryRunner.query(
+    //   `ALTER TABLE "coupon" ADD "brandDiscountId" character varying`,
+    // );
+    // await queryRunner.query(
+    //   `ALTER TABLE "coupon" ADD "brandPriceRuleId" character varying`,
+    // );
+    // await queryRunner.query(
+    //   `ALTER TABLE "order" ALTER COLUMN "orderCode" SET DEFAULT substr(uuid_generate_v4()::text, 1, 6)`,
+    // );
+    // await queryRunner.query(
+    //   `ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" TYPE numeric`,
+    // );
+    // await queryRunner.query(
+    //   `ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" SET DEFAULT '0.984'`,
+    // );
+    // await queryRunner.query(
+    //   `ALTER TABLE "admin_settings" ALTER COLUMN "topupLimitFactor" SET DEFAULT '1.2'`,
+    // );
+    // await queryRunner.query(
+    //   `ALTER TABLE "admin_settings" ALTER COLUMN "meTokenValue" SET DEFAULT '0.09'`,
+    // );
+    // await queryRunner.query(
+    //   `ALTER TABLE "auditTrail" ALTER COLUMN "userId" SET NOT NULL`,
+    // );
+    // await queryRunner.query(
+    //   `ALTER TABLE "auditTrail" ALTER COLUMN "auditType" SET NOT NULL`,
+    // );
+    // await queryRunner.query(
+    //   `ALTER TABLE "auditTrail" ALTER COLUMN "description" SET NOT NULL`,
+    // );
+    // await queryRunner.query(
+    //   `ALTER TABLE "auditTrail" ALTER COLUMN "reportableId" SET NOT NULL`,
+    // );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
