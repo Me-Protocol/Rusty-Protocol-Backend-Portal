5e8de0f771c1a90ead9e5f549507cb430f4ab87c
Emmanuel ADEBAYO <emmanueltolu.adebayo@gmail.com>
2024-04-08 10:24:55 +0100

    fix: resolved migration script

--- src/migrations/1712533901906-migrations.ts
+++ src/migrations/1712533901906-migrations.ts
@@ -1,24 +1,47 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Migrations1712533901906 implements MigrationInterface {
-    name = 'Migrations1712533901906'
+  name = 'Migrations1712533901906';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "coupon" RENAME COLUMN "orderCode" TO "order_code"`);
-        await queryRunner.query(`ALTER TABLE "order" ALTER COLUMN "orderCode" SET DEFAULT substr(uuid_generate_v4()::text, 1, 6)`);
-        await queryRunner.query(`ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" TYPE numeric`);
-        await queryRunner.query(`ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" SET DEFAULT '0.984'`);
-        await queryRunner.query(`ALTER TABLE "admin_settings" ALTER COLUMN "topupLimitFactor" SET DEFAULT '1.2'`);
-        await queryRunner.query(`ALTER TABLE "admin_settings" ALTER COLUMN "meTokenValue" SET DEFAULT '0.09'`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "coupon" RENAME COLUMN IF EXISTS "orderCode" TO "order_code"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "order" ALTER COLUMN "orderCode" SET DEFAULT substr(uuid_generate_v4()::text, 1, 6)`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" TYPE numeric`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" SET DEFAULT '0.984'`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "admin_settings" ALTER COLUMN "topupLimitFactor" SET DEFAULT '1.2'`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "admin_settings" ALTER COLUMN "meTokenValue" SET DEFAULT '0.09'`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "admin_settings" ALTER COLUMN "meTokenValue" SET DEFAULT 0.09`);
-        await queryRunner.query(`ALTER TABLE "admin_settings" ALTER COLUMN "topupLimitFactor" SET DEFAULT 1.2`);
-        await queryRunner.query(`ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" SET DEFAULT 0.984`);
-        await queryRunner.query(`ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" TYPE numeric`);
-        await queryRunner.query(`ALTER TABLE "order" ALTER COLUMN "orderCode" SET DEFAULT substr((uuid_generate_v4()), 1, 6)`);
-        await queryRunner.query(`ALTER TABLE "coupon" RENAME COLUMN "order_code" TO "orderCode"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "admin_settings" ALTER COLUMN "meTokenValue" SET DEFAULT 0.09`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "admin_settings" ALTER COLUMN "topupLimitFactor" SET DEFAULT 1.2`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" SET DEFAULT 0.984`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" TYPE numeric`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "order" ALTER COLUMN "orderCode" SET DEFAULT substr((uuid_generate_v4()), 1, 6)`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "coupon" RENAME COLUMN "order_code" TO "orderCode"`,
+    );
+  }
 }
