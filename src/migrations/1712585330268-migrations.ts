bc7f7aed692c1e1f3f82567221e245f4a70349d5
Emmanuel ADEBAYO <emmanueltolu.adebayo@gmail.com>
2024-04-11 19:07:33 +0100

    fix: resolved changes

--- src/migrations/1712585330268-migrations.ts
+++ src/migrations/1712585330268-migrations.ts
@@ -1,28 +1,57 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Migrations1712585330268 implements MigrationInterface {
-    name = 'Migrations1712585330268'
+  name = 'Migrations1712585330268';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "coupon" ADD "order_code" character varying`);
-        await queryRunner.query(`ALTER TABLE "coupon" ADD "brandDiscountId" character varying`);
-        await queryRunner.query(`ALTER TABLE "coupon" ADD "brandPriceRuleId" character varying`);
-        await queryRunner.query(`ALTER TABLE "order" ALTER COLUMN "orderCode" SET DEFAULT substr(uuid_generate_v4()::text, 1, 6)`);
-        await queryRunner.query(`ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" TYPE numeric`);
-        await queryRunner.query(`ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" SET DEFAULT '0.984'`);
-        await queryRunner.query(`ALTER TABLE "admin_settings" ALTER COLUMN "topupLimitFactor" SET DEFAULT '1.2'`);
-        await queryRunner.query(`ALTER TABLE "admin_settings" ALTER COLUMN "meTokenValue" SET DEFAULT '0.09'`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "coupon" ADD IF NOT EXISTS "order_code" character varying`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "coupon" ADD IF NOT EXISTS "brandDiscountId" character varying`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "coupon" ADD IF NOT EXISTS "brandPriceRuleId" character varying`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "order" ALTER COLUMN "orderCode" SET DEFAULT substr(uuid_generate_v4()::text, 1, 6)`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" TYPE numeric`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" SET DEFAULT '0.984'`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "admin_settings" ALTER COLUMN "topupLimitFactor" SET DEFAULT '1.2'`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "admin_settings" ALTER COLUMN "meTokenValue" SET DEFAULT '0.09'`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "admin_settings" ALTER COLUMN "meTokenValue" SET DEFAULT 0.09`);
-        await queryRunner.query(`ALTER TABLE "admin_settings" ALTER COLUMN "topupLimitFactor" SET DEFAULT 1.2`);
-        await queryRunner.query(`ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" SET DEFAULT 0.984`);
-        await queryRunner.query(`ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" TYPE numeric`);
-        await queryRunner.query(`ALTER TABLE "order" ALTER COLUMN "orderCode" SET DEFAULT substr((uuid_generate_v4()), 1, 6)`);
-        await queryRunner.query(`ALTER TABLE "coupon" DROP COLUMN "brandPriceRuleId"`);
-        await queryRunner.query(`ALTER TABLE "coupon" DROP COLUMN "brandDiscountId"`);
-        await queryRunner.query(`ALTER TABLE "coupon" DROP COLUMN "order_code"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "admin_settings" ALTER COLUMN "meTokenValue" SET DEFAULT 0.09`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "admin_settings" ALTER COLUMN "topupLimitFactor" SET DEFAULT 1.2`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" SET DEFAULT 0.984`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" TYPE numeric`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "order" ALTER COLUMN "orderCode" SET DEFAULT substr((uuid_generate_v4()), 1, 6)`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "coupon" DROP COLUMN "brandPriceRuleId"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "coupon" DROP COLUMN "brandDiscountId"`,
+    );
+    await queryRunner.query(`ALTER TABLE "coupon" DROP COLUMN "order_code"`);
+  }
 }
--- src/migrations/1712756669725-migrations.ts
+++ src/migrations/1712756669725-migrations.ts
@@ -1,38 +1,92 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Migrations1712756669725 implements MigrationInterface {
-    name = 'Migrations1712756669725'
+  name = 'Migrations1712756669725';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "auditTrail" DROP COLUMN "userName"`);
-        await queryRunner.query(`ALTER TABLE "auditTrail" DROP COLUMN "userEmail"`);
-        await queryRunner.query(`ALTER TABLE "brand" ADD "shopify_access_token" character varying`);
-        await queryRunner.query(`ALTER TABLE "brand" ADD "shopify_access_token_updated_date" TIMESTAMP`);
-        await queryRunner.query(`ALTER TABLE "order" ALTER COLUMN "orderCode" SET DEFAULT substr(uuid_generate_v4()::text, 1, 6)`);
-        await queryRunner.query(`ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" TYPE numeric`);
-        await queryRunner.query(`ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" SET DEFAULT '0.984'`);
-        await queryRunner.query(`ALTER TABLE "admin_settings" ALTER COLUMN "topupLimitFactor" SET DEFAULT '1.2'`);
-        await queryRunner.query(`ALTER TABLE "admin_settings" ALTER COLUMN "meTokenValue" SET DEFAULT '0.09'`);
-        await queryRunner.query(`ALTER TABLE "auditTrail" ALTER COLUMN "userId" SET NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "auditTrail" ALTER COLUMN "auditType" SET NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "auditTrail" ALTER COLUMN "description" SET NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "auditTrail" ALTER COLUMN "reportableId" SET NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        ALTER TABLE "auditTrail" DROP COLUMN IF EXISTS "userName";
+    `);
+    await queryRunner.query(`
+        ALTER TABLE "auditTrail" DROP COLUMN IF EXISTS "userEmail";
+    `);
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "auditTrail" ALTER COLUMN "reportableId" DROP NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "auditTrail" ALTER COLUMN "description" DROP NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "auditTrail" ALTER COLUMN "auditType" DROP NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "auditTrail" ALTER COLUMN "userId" DROP NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "admin_settings" ALTER COLUMN "meTokenValue" SET DEFAULT 0.09`);
-        await queryRunner.query(`ALTER TABLE "admin_settings" ALTER COLUMN "topupLimitFactor" SET DEFAULT 1.2`);
-        await queryRunner.query(`ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" SET DEFAULT 0.984`);
-        await queryRunner.query(`ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" TYPE numeric`);
-        await queryRunner.query(`ALTER TABLE "order" ALTER COLUMN "orderCode" SET DEFAULT substr((uuid_generate_v4()), 1, 6)`);
-        await queryRunner.query(`ALTER TABLE "brand" DROP COLUMN "shopify_access_token_updated_date"`);
-        await queryRunner.query(`ALTER TABLE "brand" DROP COLUMN "shopify_access_token"`);
-        await queryRunner.query(`ALTER TABLE "auditTrail" ADD "userEmail" character varying`);
-        await queryRunner.query(`ALTER TABLE "auditTrail" ADD "userName" character varying`);
-    }
+    // Add columns to "brand" table if they do not exist
+    await queryRunner.query(`
+        ALTER TABLE "brand" ADD COLUMN IF NOT EXISTS "shopify_access_token" character varying;
+    `);
+    await queryRunner.query(`
+        ALTER TABLE "brand" ADD COLUMN IF NOT EXISTS "shopify_access_token_updated_date" TIMESTAMP;
+    `);
 
+    await queryRunner.query(
+      `ALTER TABLE "order" ALTER COLUMN "orderCode" SET DEFAULT substr(uuid_generate_v4()::text, 1, 6)`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" TYPE numeric`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" SET DEFAULT '0.984'`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "admin_settings" ALTER COLUMN "topupLimitFactor" SET DEFAULT '1.2'`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "admin_settings" ALTER COLUMN "meTokenValue" SET DEFAULT '0.09'`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "auditTrail" ALTER COLUMN "userId" SET NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "auditTrail" ALTER COLUMN "auditType" SET NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "auditTrail" ALTER COLUMN "description" SET NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "auditTrail" ALTER COLUMN "reportableId" SET NOT NULL`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "auditTrail" ALTER COLUMN "reportableId" DROP NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "auditTrail" ALTER COLUMN "description" DROP NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "auditTrail" ALTER COLUMN "auditType" DROP NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "auditTrail" ALTER COLUMN "userId" DROP NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "admin_settings" ALTER COLUMN "meTokenValue" SET DEFAULT 0.09`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "admin_settings" ALTER COLUMN "topupLimitFactor" SET DEFAULT 1.2`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" SET DEFAULT 0.984`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "reward" ALTER COLUMN "rewardValueInDollars" TYPE numeric`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "order" ALTER COLUMN "orderCode" SET DEFAULT substr((uuid_generate_v4()), 1, 6)`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "brand" DROP COLUMN "shopify_access_token_updated_date"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "brand" DROP COLUMN "shopify_access_token"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "auditTrail" ADD "userEmail" character varying`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "auditTrail" ADD "userName" character varying`,
+    );
+  }
 }
