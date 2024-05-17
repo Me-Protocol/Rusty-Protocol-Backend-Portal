c9b14e7e7f9d6bc18f5603631372fb58acde1747
John <45017704+codemobii@users.noreply.github.com>
2024-05-17 13:45:27 +0100

    Update key-management.service.ts

--- src/globalServices/key-management/key-management.service.ts
+++ src/globalServices/key-management/key-management.service.ts
@@ -1,26 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { KMS } from 'aws-sdk';
 import { KeyIdentifier } from './entities/keyIdentifier.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { KeyIdentifierType } from '@src/utils/enums/KeyIdentifierType';
 
-const { AWS_KMS_KEY_ID, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION } =
-  process.env;
+
 
 @Injectable()
 export class KeyManagementService {
-  private readonly kms: KMS;
 
   constructor(
     @InjectRepository(KeyIdentifier)
     private readonly keyIdentifierRepo: Repository<KeyIdentifier>,
   ) {
-    this.kms = new KMS({
-      accessKeyId: AWS_ACCESS_KEY_ID,
-      secretAccessKey: AWS_SECRET_ACCESS_KEY,
-      region: AWS_REGION,
-    });
+    
   }
 
   async createKeyIdentifer(
@@ -50,13 +43,7 @@ export class KeyManagementService {
 
   async encryptKey(privateKey: string): Promise<string> {
     try {
-      const params = {
-        KeyId: 'mrk-9a19daa3db02435195457c71c327d992',
-        Plaintext: privateKey,
-      };
-
-      const result = await this.kms.encrypt(params).promise();
-      return result.CiphertextBlob.toString('base64');
+      return ""
     } catch (error) {
       console.log(error);
       throw new Error(error.message);
@@ -65,13 +52,8 @@ export class KeyManagementService {
 
   async decryptKey(encryptedKey: string): Promise<string> {
     try {
-      const params = {
-        CiphertextBlob: Buffer.from(encryptedKey, 'base64'),
-        KeyId: AWS_KMS_KEY_ID,
-      };
-
-      const result = await this.kms.decrypt(params).promise();
-      return result.Plaintext.toString('utf-8');
+      
+      return ""
     } catch (error) {
       console.log(error);
       throw new Error(error.message);
